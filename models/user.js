e6116e308e57e860e14b01a981eeddb2196641ba
Soni <Ankush.Soni@emtecinc.com>
2024-02-22 12:08:17 +0530

    added initial changes

---
+++ models/user.js
@@ -0,0 +1,22 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, 'Email already exists!'],
+    required: [true, 'Email is required!'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+  },
+  image: {
+    type: String,
+  }
+});
+
+//check if User is already present in the model, if not then only create a new model names "User"
+const User = models.User || model("User", UserSchema);
+
+export default User;
